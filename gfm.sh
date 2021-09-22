479cca08918a20a67f3fd8d2a42b1df4bffa1b87
Aman Verma <amanraoverma@gmail.com>
2021-09-22 13:27:31 -0400

    Update refs to my GitHub username.

--- gfm.sh
+++ gfm.sh
@@ -23,13 +23,14 @@ printf '<!DOCTYPE html>
 <html><head><title>Markdown Preview</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet"
-href="https://cdn.jsdelivr.net/gh/a-vrma/gfmcss@0.1.1/gfm.min.css">
+href="https://cdn.jsdelivr.net/gh/averms/gfmcss@0.1.1/gfm.min.css">
 </head><body><article class="markdown-body">
   %s
 </article></body></html>
 ' "$body" >"$html_file"
 
 echo "$html_file"
+
 if command -v open; then
     open "$html_file"
 else
